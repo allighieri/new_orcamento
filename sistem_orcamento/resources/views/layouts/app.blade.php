8631d143c3594f0f76e05f387426329da7f668e1
Weder Monteiro <agenciaolhardigital@gmail.com>
2025-08-29 01:57:21 -0300

    testando webhook

--- sistem_orcamento/resources/views/layouts/app.blade.php
+++ sistem_orcamento/resources/views/layouts/app.blade.php
@@ -22,7 +22,7 @@
     <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
         <div class="container mx-auto">
             <a class="navbar-brand" href="{{ route('dashboard') }}">
-                <i class="bi bi-calculator"></i> Orça Fácil
+                <i class="bi bi-calculator"></i> Orça Fácil!
             </a>
             
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
