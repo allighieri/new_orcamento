8e1fa1c511bc2880e7a20121f177b5c308dccbb3
Weder Monteiro <agenciaolhardigital@gmail.com>
2025-09-09 11:17:30 -0300

    deplay.sh atualizado

--- deploy.sh
+++ deploy.sh
@@ -24,16 +24,29 @@ echo "Instalando dependencias do Composer..."
 /opt/cpanel/ea-php83/root/usr/bin/php /opt/cpanel/composer/bin/composer install --no-dev --optimize-autoloader
 
 # Instala as dependencias do Node.js
-echo "Instalando dependencias do Node.js..."
-npm install
+#echo "Instalando dependencias do Node.js..."
+#npm install
 
 # Compila os arquivos de front-end com o Vite
-echo "Compilando assets com o Vite..."
-npm run build
+#echo "Compilando assets com o Vite..."
+#npm run build
 
-# Executa as migrações (seguro)
-echo "Rodando migrations..."
+# Executa as migrações e apaga o banco de dados antes
+echo "Rodando migrations e seeds..."
+#desenvolvimento
+#/opt/cpanel/ea-php83/root/usr/bin/php artisan migrate:fresh --force
+#produção
 /opt/cpanel/ea-php83/root/usr/bin/php artisan migrate --force
+
+
+echo "Executando seeds..."
+/opt/cpanel/ea-php83/root/usr/bin/php artisan db:seed --class=SuperAdminSeeder --force
+/opt/cpanel/ea-php83/root/usr/bin/php artisan db:seed --class=PaymentOptionMethodSeeder --force
+/opt/cpanel/ea-php83/root/usr/bin/php artisan db:seed --class=PaymentMethodSeeder --force
+/opt/cpanel/ea-php83/root/usr/bin/php artisan db:seed --class=CompeSeeder --force
+
+echo "Criando link simbólico..."
+/opt/cpanel/ea-php83/root/usr/bin/php artisan storage:link
 
 # Limpa todos os caches de uma vez
 echo "Limpando caches..."
